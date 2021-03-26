e282dfc3379734cd6832512485a3f728830e20fe
Daniel <groendaniel@gmail.com>
2021-03-26 19:44:00 +0100

    up

--- pages/login.vue
+++ pages/login.vue
@@ -5,7 +5,8 @@ export default {
   mounted() {
     if (process.client) {
       const instagramAppId = 249315366177273;
-      window.location.href = `https://api.instagram.com/oauth/authorize?client_id=${instagramAppId}&redirect_uri=${window.location.href}&scope=user_media,user_profile&response_type=code`;
+      const redirectUrl = "https://kapsalonanssoest.nl/auth";
+      window.location.href = `https://api.instagram.com/oauth/authorize?client_id=${instagramAppId}&redirect_uri=${redirectUrl}&scope=user_media,user_profile&response_type=code`;
     }
   },
 };
