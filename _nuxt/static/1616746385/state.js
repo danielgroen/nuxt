b8276fb19bcc46d7ceb4fa163fc36136f18fdadd
danielgroen <danielgroen@users.noreply.github.com>
2021-03-26 18:34:20 +0000

    deploy: 9134f7c2e4b29c6db69d2d42372307e0a2d953f1

--- _nuxt/static/1616746385/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616746385",layout:"default",error:a,state:{contact:{phone:"+31 35 60 20 54 6"},general:{uiState:"init"}},serverRendered:true,routePath:b,config:{baseURL:"https:\u002F\u002Fnuxtjs.org",app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
---
+++ _nuxt/static/1616783659/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616783659",layout:"default",error:a,state:{contact:{phone:"+31 35 60 20 54 6"},general:{uiState:"init"}},serverRendered:true,routePath:"\u002Flogin",config:{baseURL:"http:\u002F\u002Flocalhost:3000",app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
