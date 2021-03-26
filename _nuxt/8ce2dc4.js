20e395b94e16d464f392975e2c0c6954b6bd2a9c
danielgroen <danielgroen@users.noreply.github.com>
2021-03-26 18:41:21 +0000

    deploy: 62607d2a04ab6d500af1f17ad6b034478d9da35a

---
+++ _nuxt/8ce2dc4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{702:function(n,e,t){"use strict";t.r(e);var c={asyncData:function(n){return(0,n.redirect)("/")}},d=t(54),component=Object(d.a)(c,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
--- _nuxt/e9b5b4e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{702:function(n,e,t){"use strict";t.r(e);var c={asyncData:function(n){return(0,n.redirect)("/")}},d=t(77),component=Object(d.a)(c,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
