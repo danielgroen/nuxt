4bb39a080817bd6952c8c4f739b7d58d666c81ba
danielgroen <danielgroen@users.noreply.github.com>
2021-07-14 21:03:24 +0000

    deploy: 43417d48c90f68374e650fcdb8d17c26c966d696

--- _nuxt/e746f6b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{679:function(n,e,t){"use strict";t.r(e);var c={asyncData:function(n){return(0,n.redirect)("/")}},d=t(59),component=Object(d.a)(c,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
