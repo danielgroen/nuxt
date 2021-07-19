51f6b3e99a7d883cfb489ad5b9c4c65d4f34d06d
danielgroen <danielgroen@users.noreply.github.com>
2021-07-19 22:52:31 +0000

    deploy: 25d2792d9e74206cd6143a8a7ed41115a562c71a

--- _nuxt/static/1626734719/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626734719",layout:"default",error:a,state:{contact:{phone:"+31 35 60 20 54 6"},general:{uiState:"init"}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
